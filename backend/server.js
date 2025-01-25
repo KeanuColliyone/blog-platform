3559da7762e8d806f03d67bd9b5c2914febe0fd7
Kenan Kotzee <frostcolliyone@gmail.com>
2025-01-25 19:21:42 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://keanucolliyone.github.io/blog-platform/',}));
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 app.use((req, res, next) => {
