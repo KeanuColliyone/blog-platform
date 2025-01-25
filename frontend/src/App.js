0838ca8f1d5c36c3005cde022aca2e0a23d1d050
Kenan Kotzee <frostcolliyone@gmail.com>
2025-01-25 18:24:38 +0200

    HashRouterRouter fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouterRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Login';
 import Signup from './Signup';
 import Dashboard from './Dashboard';
